8e55029b3746a859eba390f17353a97f3d40df96
guojin <jerrychane@126.com>
2019-09-30 22:22:05 +0800

    修改.gitignore

---
+++ webpack-4/lesson2/index.js
@@ -0,0 +1,9 @@
+// ES Module 模块引入方式 默认情况下浏览器无法识别
+import Header from './header';
+import Sidebar from './sidebar';
+import Content from './content';
+
+// 面向对象编程
+new Header();
+new Sidebar();
+new Content();
