cd2fc5740dd19f7bb62d55b2c46ed2915de6ac5b
jerrychane <jerrychane@126.com>
2020-06-14 23:19:42 +0800

    2020.06.14 SUN 24WEEK Beijing 2-2 新的声明方式let（下）

--- es-learning-exercise/src/index.js
+++ es-learning-exercise/src/index.js
@@ -1,5 +1,5 @@
 // let sum = (x, y) => x + y
 // console.log(sum(4, 5))
-var a = 5
-console.log(a)
-console.log(window.a)
+// var a = 5
+// console.log(a)
+// console.log(window.a)
--- es-learning-exercise/static/2-1.js
+++ es-learning-exercise/static/2-1.js
@@ -1,1 +1,24 @@
-console.log('123')
+// function foo (a = b, b = 2) {
+//     console.log(a, b)
+// }
+// foo()
+// for (let i = 0; i < 3; i++) {
+//     console.log('循环内:' + i)
+// }
+// console.log('循环外:' + i)
+// if (false) {
+//     let a = 5
+// }
+// console.log('a', a)
+for (var i = 0; i < 3; i++) {
+    (function (j) {
+        setTimeout(function () {
+            console.log(j)
+        })
+    })(i)
+}
+for (let i = 0; i < 3; i++) {
+    setTimeout(function () {
+        console.log(i)
+    })
+} 
