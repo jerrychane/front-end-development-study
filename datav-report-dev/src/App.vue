c506d3e8aec8686910c34be017f96e6bcde5f057
jerrychane <jerrychane@126.com>
2021-05-08 22:00:25 +0800

    Introduction of 5-5 element UI and ecarts

--- datav-report-dev/src/App.vue
+++ datav-report-dev/src/App.vue
@@ -1,16 +1,23 @@
 <template>
   <div id="app">
-    <div id="nav">
-    </div>
-    <router-view/>
+    <router-view></router-view>
   </div>
 </template>
 
-<style lang="scss">
+<script>
+
+export default {
+  name: 'app',
+  components: {
+  }
+}
+</script>
+
+<style>
 html,body,#app {
   width:100%;
   height:100%;
-  margin:0;
   padding:0;
+  margin:0;
 }
 </style>
