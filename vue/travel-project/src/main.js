d139084348f6c3a5c9a443ac4af55c70132da52f
jerrychane <jerrychane@126.com>
2021-05-23 14:28:11 +0800

    2021.05.23 SUT 21WEEK BJ 1-9 Vue project warm up - project code initialization

--- vue/travel-project/src/main.js
+++ vue/travel-project/src/main.js
@@ -3,9 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+// 解决移动端1px问题
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
-
+fastClick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
