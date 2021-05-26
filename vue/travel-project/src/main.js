42fe9cc5b2b613771606b21efc68e57944719807
jerrychane <jerrychane@126.com>
2021-05-26 20:54:13 +0800

    2021.05.26 WED 22 WEEK BJ 2-5 Vue project Homepage - Homepage carousel

--- vue/travel-project/src/main.js
+++ vue/travel-project/src/main.js
@@ -2,12 +2,16 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import router from './router'
 import fastClick from 'fastclick'
 import 'styles/reset.css'
 // 解决移动端1px问题
 import 'styles/border.css'
 import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
+
+Vue.use(VueAwesomeSwiper)
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
---
+++ vue/travel-project/src/pages/home/components/Swiper.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="wrapper">
+  <swiper :options="swiperOptions">
+    <swiper-slide>
+        <img class="swiper-img" src="//img1.qunarzz.com/sight/p0/2005/39/3979f1867defec4ea3.water.jpg_600x330_2106404f.jpg" alt="八达岭" />
+    </swiper-slide>
+    <swiper-slide>
+        <img class="swiper-img" src="//img1.qunarzz.com/sight/p0/2005/30/307fa37039eab5cba3.water.jpg_600x330_8db87481.jpg" alt="颐和园" />
+    </swiper-slide>
+    <div class="swiper-pagination" slot="pagination"></div>
+  </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOptions: {}
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper
+    width 100%
+    overflow hidden
+    height 0
+    padding-bottom 55%
+    bacground #eee
+    .swiper-img
+        width 100%
+</style>
