b58f68f401f07e65a20f8fee51dc4cf9e3d39350
jerrychane <jerrychane@126.com>
2021-05-28 09:01:13 +0800

    2021.05.28 FRI 22 WEEK BJ 2-9 development of hot recommended components

---
+++ vue/travel-project/src/pages/home/components/Recommand.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <div class="title">热门推荐</div>
+    <ul>
+      <li class="item">
+          <img src="//img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_140x140_73fda71d.jpg" alt="" class="item-img" />
+        <div class="item-info border-bottom">
+          <p class="item-title">大连圣亚海洋世界</p>
+          <p class="item-desc">浪漫大连首站，浪漫的海洋主题乐园</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: "Recommand",
+  data () {
+    recommandList: [{
+      id: "0001"
+    }]
+  }
+}
+</script>
+<style lang="stylus" scoped>
+@import "~styles/mixin.styl"
+
+.title
+  margin-top 0.2rem
+  line-height 0.8rem
+  background #eee
+  text-indent 0.2rem
+.item
+  display flex
+  height 1.9rem
+  .item-img
+    width 1.7rem
+    height 1.7rem
+    padding .1rem
+  .item-info
+    flex 1
+    padding .1rem
+    min-width 0
+    .item-title
+        line-height .8rem
+        font-size .32rem
+        margin-top -.2rem
+        ellipsis()
+    .item-title
+        line-height .8rem
+        color #ccc
+        ellipsis()
+    .item-button
+        line-height .44rem
+        background #ff9300
+        margin-top .35rem
+        padding 0 .2rem
+        border-radius .06rem
+        color #fff
+</style>
