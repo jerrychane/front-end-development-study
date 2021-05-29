ece9fcdb605910bafb98c0e1d9565087ee266372
jerrychane <jerrychane@126.com>
2021-05-29 16:27:58 +0800

    2021.05.29 SAT 22 WEEK BJ 1-1 City selection page - routing configuration

--- vue/travel-project/src/pages/home/components/Header.vue
+++ vue/travel-project/src/pages/home/components/Header.vue
@@ -4,7 +4,9 @@
        <div class="iconfont back-icon">&#xe624;</div>
      </div>
      <div class="header-input"><span class="iconfont">&#xe632;</span>输入城市/景点/游玩主题</div>
-     <div class="header-right">{{this.city}}<span class="iconfont arrow-icon">&#xe64a;</span></div>
+     <router-link to="/city">
+      <div class="header-right">{{this.city}}<span class="iconfont arrow-icon">&#xe64a;</span></div>
+     </router-link>
    </div>
 </template>
 
@@ -22,9 +24,9 @@ export default {
 // 1rem = html font-size = 50px
   .header
     display flex
-    line-height .86rem
+    line-height $headerHeight
     background $bgColor
-    color #fff
+    color $whiteColor
     .header-left
       width .64rem
       float left
@@ -38,13 +40,14 @@ export default {
       margin-top .12rem
       margin-left .2rem
       padding-left .2rem
-      background #fff
+      background $whiteColor
       border-radius .1rem
       color #ccc
     .header-right
       width 1.24rem
       float right
       text-align center
+      color $whiteColor
       .arrow-icon
         font-size .24rem
         margin-left -.04rem
--- vue/travel-project/src/router/index.js
+++ vue/travel-project/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue"
 import Router from "vue-router"
 import Home from "@/pages/home/Home"
 import List from "@/pages/list/List"
+import City from "@/pages/city/City"
 
 Vue.use(Router)
 
@@ -15,6 +16,10 @@ export default new Router({
       path: "/list",
       name: "List",
       component: List
+    }, {
+      path: "/city",
+      name: "City",
+      component: City
     }
   ]
 })
