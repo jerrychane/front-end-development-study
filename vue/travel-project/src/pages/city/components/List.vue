f46f8888eb0975a6cbbede536503d44c311091c4
jerrychane <jerrychane@126.com>
2021-05-31 23:19:45 +0800

    2021.05.31 MON 23WEEK BJ Use of 1-4 better scroll and layout of alphabet

--- vue/travel-project/src/pages/city/components/List.vue
+++ vue/travel-project/src/pages/city/components/List.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="list">
+    <div class="list" ref="wrapper">
+    <div>
       <div class="area">
         <div class="title border-topbottom">当前城市</div>
         <div class="button-list">
@@ -46,11 +47,16 @@
           <div class="item border-bottom">阿拉尔</div>
         </div>
       </div>
+      </div>
     </div>
 </template>
 <script>
+import BetterScroll from "better-scroll"
 export default {
-  name: "CityList"
+  name: "CityList",
+  mounted () {
+    this.scroll = new BetterScroll(this.$refs.wrapper)
+  }
 }
 </script>
 <style lang="stylus" scoped>
@@ -73,7 +79,7 @@ export default {
   right 0
   bottom 0
   .title
-    line-height .4rem
+    line-height .54rem
     background #eee
     padding-left .2rem
     color #666
