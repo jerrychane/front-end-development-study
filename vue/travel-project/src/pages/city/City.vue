2c1f3bede99301956b99c7458e4b3621d3585c08
jerrychane <jerrychane@126.com>
2021-06-01 08:51:50 +0800

    2021.06.01 TUE 23WEEK BJ 1.6 dynamic data rendering of the page

--- vue/travel-project/src/pages/city/City.vue
+++ vue/travel-project/src/pages/city/City.vue
@@ -2,22 +2,45 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
-        <city-list></city-list>
-        <city-alphabet></city-alphabet>
+        <city-list :cities="cities" :hotCities="hotCities"></city-list>
+        <city-alphabet :cities="cities"></city-alphabet>
     </div>
 </template>
 <script>
+import axios from "axios"
 import CityHeader from "./components/Header"
 import CitySearch from "./components/Search"
 import CityList from "./components/List"
 import CityAlphabet from "./components/Alphabet"
 export default {
   name: "city",
+  data () {
+    return {
+      cities: {},
+      hotCities: []
+    }
+  },
   components: {
     CityHeader,
     CitySearch,
     CityList,
     CityAlphabet
+  },
+  mounted () {
+    this.getCityInfo()
+  },
+  methods: {
+    getCityInfo () {
+      axios.get("/api/city.json").then(this.getCityInfoSucc)
+    },
+    getCityInfoSucc (res) {
+      let data = res.data
+      console.log(data)
+      if (data.ret && data.data) {
+        this.cities = data.data.cities
+        this.hotCities = data.data.hotCities
+      }
+    }
   }
 }
 </script>
