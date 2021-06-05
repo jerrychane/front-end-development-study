97dd2baef81fafb168f8fd6e069304d14d1ac051
jerrychane <jerrychane@126.com>
2021-06-05 23:09:42 +0800

    2021.06.05 SAT 23WEEK BJ 1-1 Vue project details page - dynamic routing and banner layout

---
+++ vue/travel-project/src/common/gallery/Gallery.vue
@@ -0,0 +1,13 @@
+<template name="common-galler">
+    <div>
+        common-galler
+    </div>
+</template>
+<script>
+export default {
+    name:"CommonGallery"
+}
+</script>
+<style lang="stylus" scoped>
+
+</style>
