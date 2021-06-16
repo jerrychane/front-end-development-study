34fa5c59b1470b1524e2bcc1480911b73865d228
jerrychane <jerrychane@126.com>
2021-06-16 22:03:38 +0800

    2021.06.16 WED 25WEEK BJ 2-2 Migration of entry file and configuration change of webpack

--- vue/travel/src/main.js
+++ vue/travel/src/main.js
@@ -1,12 +1,34 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from "vue"
+import App from "./App"
+import VueAwesomeSwiper from "vue-awesome-swiper"
+import router from "./router"
+import fastClick from "fastclick"
+import store from "./store"
+import "styles/reset.css"
+// 解决移动端1px问题
+import "styles/border.css"
+import "styles/iconfont.css"
+import "swiper/dist/css/swiper.css"
+
+Vue.use(VueAwesomeSwiper)
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
+/* eslint-disable no-new */
+// old
+// new Vue({
+//   el: "#app",
+//   router,
+//   store,
+//   components: { App },
+//   template: "<App/>"
+// })
 
+// new
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app")
---
+++ vue/travel/vue.config.js
@@ -0,0 +1,7 @@
+const path = require("path")
+
+module.exports = {
+    chainWebpack: (config) => {
+        config.resolve.alias.set("styles", path.join(__dirname,"./src/assets/styles/"))
+    }
+}
